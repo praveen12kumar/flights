4224934c78779f8bc932604be51b38a4b184da69
Praveen Kumar <praveenshakya441@gmail.com>
2026-03-20 09:20:54 +0530

    Added winston to add logs in a file

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 const express = require("express");
 
-const { PORT } = require("./config/server-config");
+const { ServerConfig, Logger } = require("./config");
 const apiRoutes = require("./routes");
 
 const app = express();
 
 app.use("/api", apiRoutes);
 
-app.listen(PORT, () => {
-  console.log(`Server started on port ${PORT}`);
+app.listen(ServerConfig.PORT, () => {
+  console.log(`Server started on port ${ServerConfig.PORT}`);
 });
